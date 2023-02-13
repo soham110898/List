67dcd51df22f099b33ea71eb2f25ac9332fd23c1
soham110898 <sohampatil110898@gmail.com>
2023-02-14 01:02:15 +0530

    done

--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -4,13 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Soham git successful");
-		
-		System.out.println("oh youd did it");
+		System.out.println("git successful");
 	
-	    System.out.println("good night");
-	
-	   System.out.println("its not over until win");  
+	   System.out.println("java classes");  
 	
 	}
 	
