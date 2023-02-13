cb52500c65ae1297ed1b300a2b990c18c39387e6
soham110898 <sohampatil110898@gmail.com>
2023-02-14 00:27:30 +0530

    done succesfully

--- src/com/java/Test.java
+++ src/com/java/Test.java
@@ -5,5 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Soham git successful");
+		
+		System.out.println("oh youd did it");
+	
+	    System.out.println("good night");
+	
 	}
 }
